694ec9a5bf2cb6f38c7688ca1606027558fcf50b
HOONKH <boospa0305@gmai.com>
2023-12-12 14:52:15 +0900

    MetaMask SDK Setting

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,27 @@
+import { useSDK } from "@metamask/sdk-react";
 import { FC } from "react";
 import { Link } from "react-router-dom";
 
-const Header: FC = () => {
+interface HeaderProps {
+  account: string;
+  setAccount: React.Dispatch<React.SetStateAction<string>>;
+  // Dispatch<SetStateAction<string>> = 구조분해 문법 적용!
+}
+
+const Header: FC<HeaderProps> = ({ account, setAccount }) => {
+  // generic setStateAction 값을 바꾸는 함수.
+  const { sdk } = useSDK();
+
+  const onClickMetaMask = async () => {
+    try {
+      const accounts: any = await sdk?.connect();
+
+      setAccount(accounts[0]);
+    } catch (error) {
+      console.warn("error");
+    }
+  };
+
   return (
     <div className="bg-pink-300 p-2 flex justify-between">
       <div className="flex gap-4">
@@ -10,7 +30,17 @@ const Header: FC = () => {
         <Link to="/sale">SALE</Link>
       </div>
       <div>
-        <button>MetaMask Login</button>
+        {account ? (
+          <div className="flex gap-2">
+            <span>
+              {account.substring(0, 6)}...
+              {account.substring(account.length - 5)}
+            </span>
+            <button onClick={() => setAccount("")}>Logout</button>
+          </div>
+        ) : (
+          <button onClick={onClickMetaMask}>MetaMask Login</button>
+        )}
       </div>
     </div>
   );
