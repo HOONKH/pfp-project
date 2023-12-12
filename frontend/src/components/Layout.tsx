e1d32b8cbefd5dabaf93e758264f3e73a53ab856
HOONKH <boospa0305@gmai.com>
2023-12-12 16:26:28 +0900

    Web3 Setting

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,14 +1,57 @@
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
+import Web3 from "web3";
+import { useSDK } from "@metamask/sdk-react";
+// HOOKS
 import Header from "./Header";
-
+import mintNFtAbi from "../abis/mintNFtAbi.json";
+// FILES
+export interface MyOutletContext {
+  account: string;
+  web3: Web3;
+  mintNftContract: any;
+  // Contract<ContractAbi>;
+  // 최상위 컴포넌트에 인터페이스
+}
+// 타입을 정의해줘야하는것은 안에 제네릭으로 되어있음.
 const Layout: FC = () => {
+  const { provider } = useSDK();
+
+  const [web3, setWeb3] = useState<Web3>();
   const [account, setAccount] = useState<string>("");
+  const [mintNftContract, setMintNftContract] = useState<any>(); // <Contract<ContractAbi>> 제네릭안에 제네릭
+
+  useEffect(() => {
+    if (!provider) return;
+
+    setWeb3(new Web3(provider));
+  }, [provider]);
+
+  useEffect(() => {
+    if (!web3) return;
+
+    setMintNftContract(
+      new web3.eth.Contract(
+        mintNFtAbi,
+        "0xe7E219cBaA8886Ac5D3141AdecE453E25D369aF5"
+      )
+    );
+  }, [web3]);
+
+  // useEffect(() => {
+  //   if (!web3) return;
+
+  //   console.log(mintNftContract);
+  // }, [mintNftContract]);
+
+  // useEffect(() => {
+  //   console.log(web3);
+  // }, [web3]);
 
   return (
     <div className="bg-blue-400 min-h-screen max-w-screen-md mx-auto">
       <Header account={account} setAccount={setAccount} />
-      <Outlet />
+      <Outlet context={{ mintNftContract, setMintNftContract, account }} />
     </div>
   );
 };
--- frontend/src/pages/my.tsx
+++ frontend/src/pages/my.tsx
@@ -1,6 +1,14 @@
-import { FC } from "react";
+import { FC, useEffect } from "react";
+import { useOutletContext } from "react-router-dom";
+import { MyOutletContext } from "../components/Layout";
 
 const My: FC = () => {
+  const { mintNftContract } = useOutletContext<MyOutletContext>();
+  // Layout 에 인터페이스 export로 받아옴.
+  useEffect(() => {
+    console.log(mintNftContract);
+  }, [mintNftContract]);
+
   return <div>My</div>;
 };
 
